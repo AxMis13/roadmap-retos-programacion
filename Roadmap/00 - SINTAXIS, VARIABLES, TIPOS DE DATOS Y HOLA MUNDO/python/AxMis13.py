188cb440797c5d3ce33600b2844c2320017635f2
Alexa Padilla <alexa_mishima@proton.me>
2025-09-23 14:44:59 -0600

    Solución ejercicio 00 con Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AxMis13.py
@@ -0,0 +1,13 @@
+# Sitio oficial de Python:
+#   https://www.python.org/
+
+# Esto es un comentario en una línea.
+# En python no existe una sintaxis para comentarios en varias
+# líneas, se hace con comentarios de una línea.
+
+mi_variable = int()     # las variables son de tipo dinámico
+mi_variable = float()   # por lo que pueden almacenar cualquier
+mi_variable = bool()    # tipo de dato
+
+mi_variable = "¡Hola, Python!"
+print(mi_variable)
