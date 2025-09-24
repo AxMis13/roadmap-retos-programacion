499a078981eeb52cd8b3267541c8bfac57276140
Alexa Padilla <alexa_mishima@proton.me>
2025-09-24 12:40:27 -0600

    Actualización del fichero

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AxMis13.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AxMis13.py
@@ -2,12 +2,26 @@
 #   https://www.python.org/
 
 # Esto es un comentario en una línea.
-# En python no existe una sintaxis para comentarios en varias
-# líneas, se hace con comentarios de una línea.
+
+"""
+ Esto es un
+ comentario
+ en varias líneas
+"""
+'''
+ Esto también es
+ un comentario en
+ varias líneas
+'''
 
 mi_variable = int()     # las variables son de tipo dinámico
-mi_variable = float()   # por lo que pueden almacenar cualquier
-mi_variable = bool()    # tipo de dato
+mi_variable = float()   # por lo que pueden cambiar su tipo
+mi_variable = bool()    # de dato almacenado sin conflicto
+mi_variable = complex()
+mi_variable = list()
+mi_variable = tuple()
+mi_variable = set()
+mi_variable = dict()
 
-mi_variable = "¡Hola, Python!"
+mi_variable = "¡Hola, Python!"  # str()
 print(mi_variable)
